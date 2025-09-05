0ddf7c59f87a825f99133d613ea751d519f898cd
ZacksAryasatya <syafazaqi02@gmail.com>
2025-09-05 14:51:07 +0700

    feat/[ZAA]update file auth_controller & dashboard_controller

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -1,9 +1,5 @@
 import 'package:get/get.dart';
 
 class DashboardController extends GetxController{
-  var selectedIndex = 0.obs;
   
-  void changetabIndex(int index){
-    selectedIndex.value = index;
-  }
 }
